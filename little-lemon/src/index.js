14d480116048edd88b5b799dbef889caabaef080
sticks323 <kward2106@gmail.com>
2024-03-14 09:22:13 -0400

    Updated

--- little-lemon/src/index.js
+++ little-lemon/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import ScrollToTop from "./components/layout/ScrollToTop";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
